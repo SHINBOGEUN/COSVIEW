623bf7d4e55480efb016b4c530a19a91bb17bf3d
shin <sbg03202@naver.com>
2022-06-02 10:24:23 +0900

    20220602product

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-let flag = true;
+let navflag = true;
 
 
 
 
 
 const openCate = () => {
-  document.getElementById("cate-btn").style.width = "200px";
+  
 }
 
 
 const openNav = () => {
-    if (flag == true){
+    if (navflag == true){
       document.getElementById("mySidenav").style.width = "200px";
       document.getElementById("menu-list").style.display= 'flex';
       document.getElementById("menu-list").style.alignItems= 'center';
       document.getElementById("logo4").style.visibility= 'hidden';
-      flag = false
+      navflag = false
     }else{
       document.getElementById("mySidenav").style.width = "0";
       document.getElementById("menu-list").style.display= 'none';
       document.getElementById("logo4").style.visibility= 'visible';
-      flag = true
+      navflag = true
     }
   };
